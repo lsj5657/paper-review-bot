7ade28cccdee17f84bcaeed24e5ef7c27f994a94
lsj5657 <lsj56575@gmail.com>
2025-01-10 11:24:15 +0900

    implement summary_tool.py

---
+++ agent_tools/summary_tool.py
@@ -0,0 +1,28 @@
+from langchain.agents import tool
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+
+
+@tool
+def summarize_paper(pdf_path: str) -> str:
+    """
+    Summarizes the paper text.
+    """
+
+    loader = PyPDFLoader(pdf_path)
+    docs = loader.load()
+
+
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    prompt = PromptTemplate(
+        input_variables=["paper_text"],
+        template="Summarize the following paper text: {paper_text}"
+    )
+
+    chain = prompt | llm
+
+
+    result = chain.invoke({"paper_text": docs})
+    return  result.content
+    
